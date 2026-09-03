a5cbfb1145e0667b2b165cec0780c97ce5c849b8
Sarah Darnell <sarahd1707@gmail.com>
2026-09-03 15:45:10 -0500

    updating for meeting

--- R scripts/nsaid-heal.R
+++ R scripts/nsaid-heal.R
@@ -471,7 +471,7 @@ html_page <- tags$html(
   tags$head(tags$title("Weekly Plots")),
   tags$body(
     tags$h1("Weekly Plot Gallery"),
-    tags$p("Auto-updated from R 8.19.2026"),
+    tags$p("Auto-updated from R 9.4.2026"),
     html_images
   )
 ) 
